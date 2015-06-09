677cb7a1b30948532dbd799c0f7461efe2d24dc7
hemo87 <mjh2383@my.open.ac.uk>
2015-06-09 13:01:56 +0100

    Update du.mk

--- du.mk
+++ du.mk
@@ -1,10 +1,6 @@
 $(call inherit-product, device/lge/d855/full_d855.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
 
 PRODUCT_NAME := du_d855
 
